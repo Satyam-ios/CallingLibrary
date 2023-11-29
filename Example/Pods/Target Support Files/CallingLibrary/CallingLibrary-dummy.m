bb456b164680b99b5ef6a1b13666d65de9b64935
Satyam Kumar <satyam.kumar@vgroup.net>
2023-11-29 19:03:27 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/CallingLibrary/CallingLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CallingLibrary : NSObject
+@end
+@implementation PodsDummy_CallingLibrary
+@end
---
+++ Example/Pods/Target Support Files/Pods-CallingLibrary_Example/Pods-CallingLibrary_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CallingLibrary_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CallingLibrary_Example
+@end
